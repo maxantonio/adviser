c4d013d94496d4f0fa36ce8ba46a35bd06c7c1ae
gagarin <dayrobis@jovenclub.cu>
2016-01-27 23:32:39 +0000

    configurando base de datos y actualizando el gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126183452) do
+ActiveRecord::Schema.define(version: 20160127223413) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "people", force: :cascade do |t|
+    t.integer  "age"
+    t.string   "name"
+    t.date     "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,7 +38,7 @@ ActiveRecord::Schema.define(version: 20160126183452) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
