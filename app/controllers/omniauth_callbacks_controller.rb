0984b21beef7d7b0fcf742988fd506d80d8f218a
gagarin <dayrobis@jovenclub.cu>
2016-01-28 00:39:59 +0000

    Autenticacion con linkedIn ok

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def linkedin
+  def linkedin    
     auth = env['omniauth.auth']
     @user = User.connect_to_linkedin(request.env['omniauth.auth'], current_user)
+    
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success'
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Linkedin'
       sign_in_and_redirect @user, :event => :authentication
     else
       session['devise.linkedin_uid'] = request.env['omniauth.auth']
--- db/migrate/20160126183452_add_provider_to_users.rb
+++ db/migrate/20160126183452_add_provider_to_users.rb
@@ -2,5 +2,6 @@ class AddProviderToUsers < ActiveRecord::Migration
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
+    add_colum :users, :name, :string    
   end
 end
